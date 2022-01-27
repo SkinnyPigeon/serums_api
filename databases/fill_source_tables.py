80343f7b2ea37c15b95d565e3cd7ad4414ead378
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-27 18:01:38 +0000

    updated the insert code to stop if duplicate keys found

---
+++ databases/fill_source_tables.py
@@ -0,0 +1,61 @@
+from sqlalchemy import create_engine
+import os
+import subprocess
+import pandas as pd
+import sqlalchemy
+
+project_folder = subprocess.check_output("pwd", shell=True).\
+                            decode("utf-8").rstrip()
+
+PORT = os.getenv('PGPORT')
+PASSWORD = os.getenv('PGPASSWORD')
+
+engine = create_engine(
+    f'postgresql://postgres:{PASSWORD}@localhost:{PORT}/source'
+)
+
+directories = ['ustan']
+ustan_tables = [
+    'serums_ids',
+    'cycles',
+    'general',
+    'hospital_doctors',
+    'intentions',
+    'patients',
+    'regimes',
+    'smr01',
+    'smr06',
+    'tags',
+    'translated_tags'
+]
+
+for directory in directories:
+    print(f"DIRECTORY: {directory}")
+    csv_path = f'{project_folder}/databases/data/{directory}/'
+
+    if directory == 'ustan':
+        tables = ustan_tables
+
+    for table in tables:
+        try:
+            with open(f"{csv_path}{table}.csv", 'r') as csv:
+                df = pd.read_csv(csv)
+            df.to_sql(table,
+                      con=engine,
+                      index=False,
+                      schema=directory,
+                      if_exists='append')
+        except sqlalchemy.exc.DataError as d:
+            with open(f"{csv_path}{table}.csv", 'r') as csv:
+                df = pd.read_csv(csv, escapechar='\\')
+                df.to_sql(table,
+                          con=engine,
+                          if_exists='append',
+                          index=False,
+                          schema=directory,
+                          dtype={'tag': sqlalchemy.types.JSON})
+        except sqlalchemy.exc.IntegrityError as i:
+            print("DATA HAS ALREADY BEEN INSERTED!")
+            break
+
+engine.dispose()
