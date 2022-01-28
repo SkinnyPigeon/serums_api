2417ecc4f27b3febe84582caf97ab2115543173b
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-28 16:33:48 +0000

    removed code from main

--- main.py
+++ main.py
@@ -3,6 +3,3 @@ from components.staff.departments import get_department_of_staff_member
 # app = FastAPI(
 #     title="Serums Datalake API"
 # )
-
-check = get_department_of_staff_member('ustan', 455)
-print(check)
