dd8b8f6afc3da6ea2b5e55027c18c85fae75b954
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-28 08:54:00 +0000

    scaffolded departments file

---
+++ components/staff/departments.py
@@ -0,0 +1,13 @@
+from components.connection.create_connection import setup_connection
+
+
+def get_department_of_staff_member():
+    pass
+
+
+def get_departments():
+    pass
+
+
+def check_staff_member():
+    pass
