42aad6453fd6ad2d0326d2932f2b2f7ffaae8feb
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-31 22:12:18 +0000

    can process satellites

--- tests/test_data_vault.py
+++ tests/test_data_vault.py
@@ -1,7 +1,11 @@
 from py import process
 from components.data_vaults.data_vault import generate_boilerplate, \
                                               get_id_columns
-from components.data_vaults.satellites import process_value
+from components.data_vaults.satellites import process_value, \
+                                              process_satellites
+from components.sphr.get_source_data import get_patient_data, \
+                                            parse_sphr
+from tests.test_valid_staff_jwt import right_jwt as staff_jwt
 import datetime
 import decimal
 
@@ -88,3 +92,99 @@ def test_process_value():
     assert val_two == "Hello"
     assert val_three == '11:38:48'
     assert val_four == 12378.122378237273
+
+
+def test_process_satellites():
+    data, _ = get_patient_data(
+        117,
+        ['ustan'],
+        ['patient_details', 'medication', 'wearble'],
+        staff_jwt
+    )
+    result = process_satellites(data)
+    expected = {
+        'USTAN': {
+            'ustan.general': {
+                'links': [
+                    'time_person_link',
+                    'time_location_link',
+                    'time_event_link',
+                    'person_location_link',
+                    'person_event_link',
+                    'location_event_link'
+                ],
+                'sat_time_general_details': {
+                    'hub': 'hub_time',
+                    'data': [
+                        {
+                            'first_seen_date': '18/04/2017 00:00:00',
+                            'dat_death': None
+                        }
+                    ],
+                    'keys': [
+                        {
+                            'chi': 1005549224
+                        }
+                    ]
+                },
+                'sat_person_general_patient': {
+                    'hub': 'hub_person',
+                    'data': [
+                        {
+                            'name': 'HERMIONE KOCZUR',
+                            'date_of_birth': '10/05/1954 00:00:00',
+                            'dob': '10/05/1954 00:00:00',
+                            'gender': 2,
+                            'religion': 0,
+                            'civil_st': 9,
+                            'postcode': 'KY953HY'
+                        }
+                    ],
+                    'keys': [
+                        {
+                            'chi': 1005549224
+                        }
+                    ]
+                },
+                'sat_person_general_gp': {
+                    'hub': 'hub_person',
+                    'data': [{}],
+                    'keys': [
+                        {
+                            'chi': 1005549224
+                        }
+                    ]
+                },
+                'sat_location_general_details': {
+                    'hub': 'hub_location',
+                    'data': [
+                        {
+                            'ref_hospital': 617
+                        }
+                    ],
+                    'keys': [
+                        {
+                            'chi': 1005549224
+                        }
+                    ]
+                },
+                'sat_event_general_details': {
+                    'hub': 'hub_event',
+                    'data': [
+                        {
+                            'smid': None,
+                            'smid1': None,
+                            'death_flag': 0
+                        }
+                    ],
+                    'keys': [
+                        {
+                            'chi': 1005549224
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    assert type(result) == dict
+    assert result == expected
