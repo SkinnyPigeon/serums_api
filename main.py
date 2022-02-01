b41a795424a608d1267fafa020c2d59981ee14ad
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-02-01 00:16:05 +0000

    added first staff endpoint with authorization

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header
 from auth.auth_handler import JWTBearer
-from models.request_fields import HelloResponse
+from models.request_fields import HelloResponse, \
+                                  StaffMemberDepartmentResponse
+from components.staff.departments import get_departments
+from components.staff.verify_staff_member import get_department_of_staff_member
+from components.jwt.validate import validate_jwt
+
 
 description = """
 This is an updated version of the Serums Datalake API. \
@@ -79,3 +84,22 @@ app = FastAPI(
 @app.get("/hello/hello", response_model=HelloResponse, tags=['HELLO'])
 def say_hello():
     return {"hello": "Welcome to the API. The server is on"}
+
+
+@app.get("/staff_tables/departments", tags=['STAFF'])
+def request_get_departments():
+    # jwt = request
+    pass
+
+
+@app.get("/staff_tables/get_department_of_staff_member",
+         tags=['STAFF'],
+         response_model=StaffMemberDepartmentResponse,
+         dependencies=[Depends(JWTBearer())])
+def request_get_dpt_of_staff_member(Authorization: str = Header(None)):
+    jwt_response = validate_jwt(Authorization)
+    response = get_department_of_staff_member(
+        jwt_response['hospital_id'],
+        jwt_response['serums_id']
+    )
+    return response
--- models/request_fields.py
+++ models/request_fields.py
@@ -6,3 +6,11 @@ import json
 
 class HelloResponse(BaseModel):
     __root__: dict = {"hello": "Welcome to the API. The server is on"}
+
+
+class StaffMemberDepartmentResponse(BaseModel):
+    serums_id: int = 120
+    staff_id: int = 120
+    name: str = "Charlotte Watson"
+    department_id: int = 4
+    department_name: str = "MEDICAL_STAFF"
